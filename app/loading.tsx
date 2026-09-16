292e20541964282b183717f87ea0526cd225f17f
pankaj-kakadiya <pankajkakadiya919@gmail.com>
2026-09-16 10:28:35 +0530

    Add buyer loading state

---
+++ app/loading.tsx
@@ -0,0 +1,3 @@
+export default function Loading(){
+ return <main className="nisPageLoading" aria-busy="true" aria-label="Loading page"><section className="nisLoadingCard"><div className="nisSkeleton title" style={{width:'52%'}}/><div className="nisSkeleton text"/><div className="nisSkeleton text" style={{width:'84%'}}/><div className="nisGrid3" style={{marginTop:10}}><div className="nisSkeleton media"/><div className="nisSkeleton media"/><div className="nisSkeleton media"/></div></section></main>
+}
